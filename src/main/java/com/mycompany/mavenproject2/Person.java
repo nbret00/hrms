3198567ef64036da6f37c65936f5158267fc0345
nbret00 <nino.bretana@safeway.com>
2016-07-22 10:55:02 +0800

    make it better

---
+++ src/main/java/com/mycompany/mavenproject2/Person.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject2;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author nbret00
+ */
+@Entity
+@Table(name = "\"Person\"", schema = "public")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Person.findAll", query = "SELECT p FROM Person p"),
+    @NamedQuery(name = "Person.findByFirstname", query = "SELECT p FROM Person p WHERE p.firstname = :firstname"),
+    @NamedQuery(name = "Person.findByLastname", query = "SELECT p FROM Person p WHERE p.lastname = :lastname"),
+    @NamedQuery(name = "Person.findByPersonID", query = "SELECT p FROM Person p WHERE p.personID = :personID")})
+public class Person implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Size(max = 2147483647)
+    @Column(name = "\"Firstname\"")
+    private String firstname;
+    @Size(max = 2147483647)
+    @Column(name = "\"Lastname\"")
+    private String lastname;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "\"PersonID\"")
+    private Integer personID;
+
+    public Person() {
+    }
+
+    public Person(Integer personID) {
+        this.personID = personID;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Integer getPersonID() {
+        return personID;
+    }
+
+    public void setPersonID(Integer personID) {
+        this.personID = personID;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (personID != null ? personID.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Person)) {
+            return false;
+        }
+        Person other = (Person) object;
+        if ((this.personID == null && other.personID != null) || (this.personID != null && !this.personID.equals(other.personID))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.mycompany.mavenproject2.Person[ personID=" + personID + " ]";
+    }
+    
+}
---
+++ target/generated-sources/annotations/com/mycompany/mavenproject2/Person_.java
@@ -0,0 +1,15 @@
+package com.mycompany.mavenproject2;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-07-22T10:33:37")
+@StaticMetamodel(Person.class)
+public class Person_ { 
+
+    public static volatile SingularAttribute<Person, String> lastname;
+    public static volatile SingularAttribute<Person, String> firstname;
+    public static volatile SingularAttribute<Person, Integer> personID;
+
+}
