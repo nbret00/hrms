8caa12cdb2773eaec35110dbb357e98bf0eafbcc
nbret00 <nino.bretana@safeway.com>
2016-07-25 16:29:20 +0800

    make it better

--- target/generated-sources/annotations/com/mycompany/mavenproject2/Person_.java
+++ target/generated-sources/annotations/com/mycompany/mavenproject2/Person_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-07-22T10:33:37")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-07-25T16:14:57")
 @StaticMetamodel(Person.class)
 public class Person_ { 
 
